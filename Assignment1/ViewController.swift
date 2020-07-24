1e2fdec763dbb48c00e3127a7fc5f3553e84726a
[Prabhav Mehra s3743761] <s3743761@student.rmit.edu.au>
2020-07-24 19:26:10 +1000

    Initial Commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Prabhav Mehra on 24/07/20.
+//  Copyright © 2020 Prabhav Mehra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assignment1Tests/Assignment1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment1Tests.swift
+//  Assignment1Tests
+//
+//  Created by Prabhav Mehra on 24/07/20.
+//  Copyright © 2020 Prabhav Mehra. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment1
+
+class Assignment1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
