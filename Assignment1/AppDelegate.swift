d66a7d831550aba821213a93f6c1a8e610c61862
[Prabhav Mehra s3743761] <s3743761@student.rmit.edu.au>
2020-07-25 19:44:45 +1000

    Connecting to database and storig users in firebase database

--- Assignment1/AppDelegate.swift
+++ Assignment1/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 
 @UIApplicationMain
@@ -15,10 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-            FirebaseApp.configure()
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
